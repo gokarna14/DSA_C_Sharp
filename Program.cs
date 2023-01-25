6163b450ee3188746fd3cb98bfcf3622b624233d
System Administrator <gokarna.adhikari14@gmail.com>
2023-01-25 16:16:57 +0545

    The hello world

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace DSA
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("\n\n");
+
+            Console.WriteLine("Hello World !!");
+
+
+            System.Console.WriteLine("\n\n");
+        }
+    }
+}
